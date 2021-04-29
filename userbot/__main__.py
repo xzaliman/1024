ac590e1e8bc5b7fa0741e2b653065a7057a1f347
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-04-29 11:25:35 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Man-Userbot ⚙️ V{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    f"Userbot ⚙️ V{BOT_VER} [🔥 Userbot ONLINE 🔥]")
 
 
 if len(argv) not in (1, 3, 4):
